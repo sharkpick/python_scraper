cb685f3625a4fc47a2f1ab6c8ee0c96ecd695cf3
william <will@pop-os.localdomain>
2019-12-10 12:26:23 -0500

    full base scraper

--- scraper.py
+++ scraper.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import urllib.request, urllib.error, urllib.parse
+import re
+
+jw_spam = "https://joewein.net/dl/bl/dom-bl.txt"
+response = urllib.request.urlopen(jw_spam)
+raw_bl = response.read().decode("utf-8")
+blacklist = raw_bl.split("\n")
+for x in blacklist:
+	print(re.sub(r"\;\d+", "",x))
