5114492148d66df3b05408d015e5022f4ca67372
Will Dillon <willamdillon+squishd@gmail.com>
2019-12-10 16:43:02 -0500

    ammending commit

--- scraper.py
+++ scraper.py
@@ -3,8 +3,8 @@ import urllib.request, urllib.error, urllib.parse
 import re
 
 jw_spam = "https://joewein.net/dl/bl/dom-bl.txt"
-response = urllib.request.urlopen(jw_spam)
-raw_bl = response.read().decode("utf-8")
+with urllib.request.urlopen(jw_spam) as response:
+	raw_bl = response.read().decode("utf-8")
 blacklist = raw_bl.split("\n")
 for x in blacklist:
 	print(re.sub(r"\;\d+", "",x))
