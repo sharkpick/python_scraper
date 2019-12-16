636a4bb243fe9ea74ed09dd3d62a26818bdfa71f
william <will@pop-os.localdomain>
2019-12-16 09:38:18 -0500

    full working scraper, no error handling yet

--- scraper.py
+++ scraper.py
@@ -1,33 +1,48 @@
 #!/usr/bin/python3
 import urllib.request, urllib.error, urllib.parse
 import re, time, os, glob
+from urllib.request import Request, urlopen
 
-# quick, grab last (new) raw BL, before we make any new files
-raw_list = glob.glob('/home/will/data/raw/*')
-yesterday_bl = max(raw_list, key=os.path.getctime)
-#print(yesterday_bl) # debug
-# make some timestamps
+def diff(yesterday, blacklist):
+	return(list(set(yesterday) - (set(blacklist))))
+
+def malbytes_pull():
+	mal_bytes = "https://blog.malwarebytes.com/detection/domain/"
+	mal_regex = re.compile('<li><a href="https://blog.malwarebytes.com/detections/.+?/">(.+?)</a></li>')
+	req = Request(mal_bytes, headers={'User-Agent': 'Mozilla/5.0'})
+	raw_bl = urlopen(req).read().decode('utf-8')
+	return(mal_regex.findall(raw_bl))
+
+def jw_spam():
+	jw_spam = "https://joewein.net/dl/bl/dom-bl.txt"
+	with urllib.request.urlopen(jw_spam) as response:
+		raw_bl = response.read().decode("utf-8")
+	raw_bl= re.sub(r"\;\d+", "", raw_bl)
+	return(raw_bl.split("\n"))
+# make a timestamp
 ts = time.gmtime()
 raw_ts = "/home/will/data/raw/raw-" +time.strftime("%Y%m%d", ts)
 diff_ts = "/home/will/data/blacklists/bl-" +time.strftime("%Y%m%d", ts)
-# pull some domains
-jw_spam = "https://joewein.net/dl/bl/dom-bl.txt"
-with urllib.request.urlopen(jw_spam) as response:
-	raw_bl = response.read().decode("utf-8")
-blacklist = raw_bl.split("\n")
-# write formatted domains to file
-with open(raw_ts, "w") as f:
+raw_list = glob.glob('/home/will/data/raw/*')
+yesterday_bl = max(raw_list, key=os.path.getctime)
+# make var blacklist
+blacklist = set()
+for x in jw_spam():
+	blacklist.add(x)
+for y in malbytes_pull():
+	blacklist.add(y)
+blacklist = list(blacklist)
+blacklist.sort()
+# write to file
+with open(raw_ts, 'w') as f:
 	for x in blacklist:
-		f.write(re.sub(r"\;\d+", "",x) + "\n")
-# find diff - get yesterday's BL
-with open(yesterday_bl, "r") as f:
+		f.write(x + "\n")
+# grab var yesterday for diff
+with open(yesterday_bl, 'r') as f:
 	raw = f.read()
 yesterday = raw.split("\n")
-
-def Diff(yesterday, blacklist):
-	return(list(set(yesterday) - set(blacklist)))
-new_diff = Diff(yesterday, blacklist)
+new_diff = diff(yesterday, blacklist)
 new_diff.sort()
-with open(diff_ts, "w") as f:
+with open(diff_ts, 'w') as f:
 	for x in new_diff:
 		f.write(x + "\n")
